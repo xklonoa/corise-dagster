b535c39241da9fb0db8a549869baf4ee104a83a8
xin L <myspamz@gmail.com>
2023-04-25 02:42:59 +0000

    passed tests

--- week_3/workspaces/project/week_3.py
+++ week_3/workspaces/project/week_3.py
@@ -12,6 +12,7 @@ from dagster import (
     ScheduleDefinition,
     SensorEvaluationContext,
     SkipReason,
+    String,
     graph,
     op,
     schedule,
@@ -24,29 +25,36 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(required_resource_keys={"s3"},
+    config_schema={"s3_key": String})
+def get_s3_data(context: OpExecutionContext) -> List[Stock]:
+    s3_key = context.op_config['s3_key']
+    stocks = [Stock.from_list(stock) for stock in context.resources.s3.get_data(s3_key)]
+    return stocks
 
 
 @op
-def process_data():
-    pass
+def process_data(context: OpExecutionContext, stocks: List[Stock]) -> Aggregation:
+    sorted_stocks = sorted(stocks, key=lambda s: s.high, reverse=True)
+    max_stock = sorted_stocks[0]
+    return Aggregation(date=max_stock.date, high=max_stock.high)
 
 
-@op
-def put_redis_data():
-    pass
+@op(required_resource_keys={"redis"})
+def put_redis_data(context: OpExecutionContext, aggregation: Aggregation) -> Nothing:
+    context.resources.redis.put_data(name=aggregation.date.strftime("%y%m%d"), value=str(aggregation.high))
 
 
-@op
-def put_s3_data():
-    pass
-
+@op(required_resource_keys={"s3"})
+def put_s3_data(context: OpExecutionContext, aggregation: Aggregation) -> Nothing:
+    context.resources.s3.put_data(key_name=aggregation.date.strftime("%y%m%d"), data=aggregation)
 
 @graph
 def machine_learning_graph():
-    pass
+    stocks = get_s3_data()
+    aggregation = process_data(stocks)
+    put_redis_data(aggregation)
+    put_s3_data(aggregation)
 
 
 local = {
@@ -62,28 +70,55 @@ docker = {
     "ops": {"get_s3_data": {"config": {"s3_key": "prefix/stock_9.csv"}}},
 }
 
-
-def docker_config():
-    pass
+@static_partitioned_config(partition_keys=[str(pk) for pk in range(1,11)])
+def docker_config(partition_key: int):
+    return {
+    "resources": {
+        "s3": {"config": S3},
+        "redis": {"config": REDIS},
+    },
+    "ops": {"get_s3_data": {"config": {"s3_key": f"prefix/stock_{partition_key}.csv"}}},
+    }
 
 
 machine_learning_job_local = machine_learning_graph.to_job(
     name="machine_learning_job_local",
+    config=local,
+    resource_defs={'s3': mock_s3_resource, 'redis': ResourceDefinition.mock_resource()}
 )
 
 machine_learning_job_docker = machine_learning_graph.to_job(
     name="machine_learning_job_docker",
+    config=docker,
+    resource_defs={'s3': s3_resource, 'redis': redis_resource},
+    op_retry_policy=RetryPolicy(max_retries=10,  delay=1)
 )
 
 
-machine_learning_schedule_local = None
+
+machine_learning_schedule_local = ScheduleDefinition(cron_schedule="*/15 * * * *", job=machine_learning_job_local)
 
 
-@schedule
+@schedule(cron_schedule="0 * * * *", job=machine_learning_job_docker)
 def machine_learning_schedule_docker():
-    pass
+    for partition_key in docker_config.get_partition_keys():
+        yield RunRequest(run_key=partition_key, run_config=docker_config.get_run_config(partition_key))
 
 
-@sensor
+@sensor(job=machine_learning_job_docker)
 def machine_learning_sensor_docker():
-    pass
+    s3_keys = get_s3_keys(bucket="dagster", prefix="prefix", endpoint_url="http://localstack:4566")
+    if not s3_keys:
+            yield SkipReason("No new s3 files found in bucket.")
+    else:
+        for key in s3_keys:
+            yield RunRequest(
+                run_key=key,
+                run_config={
+                    "resources": {
+                        "s3": {"config": S3},
+                        "redis": {"config": REDIS},
+                    },
+                    "ops": {"get_s3_data": {"config": {"s3_key": key}}},
+}
+            )
